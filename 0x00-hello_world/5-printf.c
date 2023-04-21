7c0d7f2f6c6f4775c4811dc0b8530adde0246bea
lucykago <luciekago1@gmail.com>
2023-04-21 13:39:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the fuction
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n
+")
+	return (0);
+}
